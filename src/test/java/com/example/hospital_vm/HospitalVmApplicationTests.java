c7bc2ae0993b9dc79d12a01629799435bda1ae4b
Nicolas <nicolas.programador@gmail.com>
2025-04-23 20:39:24 -0400

    first commit

---
+++ src/test/java/com/example/hospital_vm/HospitalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital_vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalVmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
