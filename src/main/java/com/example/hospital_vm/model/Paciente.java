9d9ae299f4d06dae8ad7675974edda188608703c
Sebastian <sebastianmondaca95@gmail.com>
2025-04-24 02:06:35 -0400

    Actualizacion 1.2

--- src/main/java/com/example/hospital_vm/model/Paciente.java
+++ src/main/java/com/example/hospital_vm/model/Paciente.java
@@ -1,5 +1,36 @@
 package com.example.hospital_vm.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Table(name = "paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Paciente {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique= true, length=13,nullable=false)
+    private String run;
+
+    @Column(nullable=false)
+    private String nombres;
+
+    @Column(nullable=false)
+    private String apellidos;
+
+    @Column(nullable=true)
+    private Date fechaNacimiento;
+
+    @Column(nullable=false)
+    private String correo;
 }
